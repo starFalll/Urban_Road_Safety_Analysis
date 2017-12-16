bcb02944916c3f1932c418b4fea099a628d8934c
starFalll <fyxformal@gmail.com>
2017-12-16 21:41:06 +0800

    完成了后端算法,还差FactorMapper.xml中的多表映射没有完成

--- web_first/src/main/java/team/web_first/javabean/FactorAll.java
+++ web_first/src/main/java/team/web_first/javabean/FactorAll.java
@@ -2,6 +2,14 @@ package team.web_first.javabean;
 
 import java.io.Serializable;
 
+/**
+ * 对应数据库中四个TABLE
+ * risk_perception
+ * dangerous_driving
+ * confidence
+ * personality
+ *
+ */
 public class FactorAll implements Serializable {
     private int riskPerceptionId;
     private int getRiskPerceptionScore;
@@ -27,6 +35,11 @@ public class FactorAll implements Serializable {
     private int PersonalityScore;
 
     private boolean D1, D2, D3, D4, D5, D6;
+
+    public boolean[] getAllBooleanValue(){
+        boolean[] All={A1,A2,A3,A4,A5,B1, B2, B3, B4, B5, B6,C1, C2, C3, C4, C5, C6,D1, D2, D3, D4, D5, D6};
+        return All;
+    }
 
     public int getRiskPerceptionId() {
         return riskPerceptionId;
